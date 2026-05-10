e2ec5995af1b02575316c278d6942c506f136851
Suman Ranjan <sumanranjan17291729@gmail.com>
2026-05-10 11:32:55 +0530

    Refactor CORS middleware configuration and remove deployment readiness code

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -15,7 +15,10 @@ import sessionRoutes from "./routes/sessionRoutes.js"
 const app = express()
 
 app.use(express.json())
-app.use(cors({origin : ENV.CLIENT_URL , credentials : true })) // credentials true mean => server allows a browers to include  cookies on request
+app.use(cors({
+  origin: [ENV.CLIENT_URL],
+  credentials: true,
+})); // credentials true mean => server allows a browers to include  cookies on request
 app.use(clerkMiddleware()) // this adds auth field to req obj : req.auth()
 
 app.use("/api/inngest" , serve({client : inngest , functions}))
@@ -27,15 +30,6 @@ const __dirname = path.resolve()
 app.get("/hello" , (req , res)=>{
     res.status(200).json({msg : "success form apiss"})
 })
-
-//make ready for deploy
-if(ENV.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname , "../FrontEnd/dist")))
-
-    app.get("/{*any}" , (req , res)=>{
-        res.sendFile(path.join(__dirname , "../FrontEnd" , "dist" , "index.html"))
-    })
-}
 
 const startServer = async () => {
   try {
